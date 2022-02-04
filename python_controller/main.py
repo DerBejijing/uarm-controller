0e7cbb7051e27c89749935b8d2bf10e93f9af3af
Der Bejijing <62348565+DerBejijing@users.noreply.github.com>
2022-02-04 22:05:56 +0100

    Update main.py

--- python_controller/main.py
+++ python_controller/main.py
@@ -8,7 +8,7 @@ import random
 from controller import arduino
 from controller import mainframe
 
-os.system("rm -f log.txt")
+
 
 s = arduino.SerialArduino("/dev/ttyACM0", 9600)
 m = mainframe.Mainframe()
@@ -16,32 +16,6 @@ m.set_arduino(s)
 
 s.set_mainframe(m)
 s.start()
-
 s.block_ready()
 
-
-s.wait_run_command("S_L G1 1")
-time.sleep(1)
-s.wait_run_command("S_L G1 0")
-
-
 m.start()
-
-# sys.exit()
-
-
-# while True:
-# 	r1 = random.randint(0, 1)
-# 	r2 = random.randint(0, 1)
-
-# 	command = "S_L"
-
-# 	if r1 == 0: command += " R1"
-# 	else: command += " G1"
-
-# 	command += " {}".format(r2)
-
-# 	s.wait_run_command(command)
-# 	print(command)
-
-# print("done")
