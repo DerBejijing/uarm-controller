5efc13e2911fcc147238c25b66c25d8e0d723196
Der Bejijing <62348565+DerBejijing@users.noreply.github.com>
2022-02-06 19:20:41 +0100

    Update main.py

--- python_controller/main.py
+++ python_controller/main.py
@@ -7,12 +7,15 @@ import random
 
 from controller import arduino
 from controller import mainframe
-
+from controller import uarm
 
 
 s = arduino.SerialArduino("/dev/ttyACM0", 9600)
+u = uarm.SerialUarm("/dev/ttyACM1", 115200)
+
 m = mainframe.Mainframe()
 m.set_arduino(s)
+m.set_uarm(u)
 
 s.set_mainframe(m)
 s.start()
